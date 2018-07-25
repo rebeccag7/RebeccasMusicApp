701f7e7fa4abdf179efdfee663976c3db669e06c
Rebecca Goveia <rebeccagoveia@gmail.com>
2018-07-25 17:58:55 -0400

    Added code to check if signup was successful

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -13,6 +13,13 @@
 			$this->validateLastName($ln);
 			$this->validateEmails($em, $em);
 			$this->validatePasswords($pw, $pw2);
+
+			if (empty($this->errorArray)) {
+				// Insert into db
+				return true;
+			} else {
+				return false;
+			}
 		}
 
 		private function validateUsername($un) {
