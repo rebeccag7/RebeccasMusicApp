bb3fe20f66e871d6e59393974872e092ae5cf01d
Rebecca Goveia <rebeccagoveia@gmail.com>
2018-08-04 00:00:46 -0400

    Created users table and connection to db

---
+++ includes/config.php
@@ -0,0 +1,12 @@
+<?php
+	ob_start();
+
+	$timezone = date_default_timezone_set("America/New_York");
+
+	$con = mysqli_connect("localhost", "root", "", "rebecca's music app");
+
+	if(mysqli_connect_errno()) {
+		echo "Failed to connect: " . mysqli_connect_errno();
+	}
+
+?>
