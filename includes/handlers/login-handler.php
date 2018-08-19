49b8dd92931a0a5152eb8b839ea1ade9170cf69c
Rebecca Goveia <rebeccagoveia@gmail.com>
2018-08-19 17:59:20 -0400

    Added methods for login handler

--- includes/handlers/login-handler.php
+++ includes/handlers/login-handler.php
@@ -1,5 +1,14 @@
 <?php
 if(isset($_POST['loginButton'])) {
 	// Login button was pressed.
+	$username = $_POST['loginUsername'];
+	$password = $_POST['loginPassword'];
+
+	// Login
+	$result = $account->login($username, $password);
+
+	if ($result == true) {
+		header("Location: index.php");
+	}
 }
 ?>
